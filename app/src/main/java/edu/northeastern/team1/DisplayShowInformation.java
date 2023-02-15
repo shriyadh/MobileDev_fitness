987a808eeb5a458ef2d47d81bae486fd0bedb899
shriyadh03 <shriyadh03.gmail.com>
2023-02-15 14:11:46 -0500

    Merged mariahs branch

---
+++ app/src/main/java/edu/northeastern/team1/DisplayShowInformation.java
@@ -0,0 +1,9 @@
+package edu.northeastern.team1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+public class DisplayShowInformation extends AppCompatDialogFragment {
+
+
+}
