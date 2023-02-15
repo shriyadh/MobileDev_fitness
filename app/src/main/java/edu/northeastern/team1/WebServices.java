e28c84d9b274dbb6e7d57ab94e28562f45c14db3
Mariah Maynard <98000652+mariah-may23@users.noreply.github.com>
2023-02-15 13:59:52 -0500

    able to retrieve all needed fields from api

--- app/src/main/java/edu/northeastern/team1/WebServices.java
+++ app/src/main/java/edu/northeastern/team1/WebServices.java
@@ -24,12 +24,6 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class WebServices extends AppCompatActivity {
-    private Handler textHandler = new Handler();
-
-
-    private static final String TAG = "WebServiceActivity";
-
-
     ArrayList<String> shows = new ArrayList<>();
 
     @Override
@@ -40,9 +34,6 @@ public class WebServices extends AppCompatActivity {
     }
 
     public void callApi(View v){
-        // store users show
-       // String str = mURLEditText.getText().toString();
-        //String str = mURLEditText.getText().toString();
         String str = "https://api.tvmaze.com/search/shows?q=sunny%20in";
         runnableThread runnableThread = new runnableThread();
         runnableThread.setURL(str);
@@ -62,25 +53,67 @@ public class WebServices extends AppCompatActivity {
         public void run() {
 
             try {
+                // store search as a url
                 URL url = new URL(search);
                 System.out.println(url);
 
+                // set up connection
                 HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                System.out.println("first");
                 conn.setRequestMethod("GET");
+
+                // app will read data from the URL connection
                 conn.setDoInput(true);
 
                 conn.connect();
-                System.out.println("Connected");
 
-            } catch (MalformedURLException e) {
-                Log.e(TAG, "MalformedURLException");
-                e.printStackTrace();
-            } catch (ProtocolException e) {
-                Log.e(TAG, "ProtocolException");
-                e.printStackTrace();
-            } catch (IOException e) {
-                Log.e(TAG, "IOException");
+
+                // get input from api and convert to string
+                InputStream inputStream = conn.getInputStream();
+                final String resp = StreamToString(inputStream);
+
+
+                // Store array of objects that api returns
+                JSONArray jArray = new JSONArray(resp);
+
+                // loop through the array
+                for (int i = 0; i < jArray.length(); i++ ) {
+                    // store the first object in the array
+                    JSONObject obj = jArray.getJSONObject(i);
+                    System.out.println(obj);
+
+                    // the object contains another object called "show"
+                    // store the show object
+                    JSONObject show = obj.getJSONObject("show");
+
+                    // get show name
+                    String name = show.getString("name");
+                    System.out.println(name);
+
+                    // get show description
+                    String description = show.getString("summary");
+                    System.out.println(description);
+
+                    // get average rating
+                    JSONObject rating = show.getJSONObject("rating");
+                    String avg_rating = rating.getString("average");
+                    System.out.println(avg_rating);
+
+                    // get image link
+                    JSONObject image = show.getJSONObject("image");
+                    String img_link = image.getString("medium");
+                    System.out.println(img_link);
+
+                    //get premiere date
+                    String date = show.getString("premiered");
+                    System.out.println(date);
+
+                    //making sure shows are stored
+                    //will need to store show objects instead
+                    shows.add(name);
+
+                    System.out.println(shows);}
+
+            } catch (IOException | JSONException e) {
                 e.printStackTrace();
             }
         }
@@ -88,6 +121,9 @@ public class WebServices extends AppCompatActivity {
 
     }
 
-
+    private String StreamToString(InputStream i) {
+        Scanner s = new Scanner(i).useDelimiter("\\A");
+        return s.hasNext() ? s.next() : "";
+    }
 
 }
