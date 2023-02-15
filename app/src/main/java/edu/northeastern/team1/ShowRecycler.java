22d620dc6a7e93872171510f56683c083df1cb8d
shriyadh03 <shriyadh03.gmail.com>
2023-02-15 13:43:58 -0500

    Basic set up for recycler

--- app/src/main/java/edu/northeastern/team1/ShowRecycler.java
+++ app/src/main/java/edu/northeastern/team1/ShowRecycler.java
@@ -2,7 +2,10 @@ package edu.northeastern.team1;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -22,9 +25,77 @@ public class ShowRecycler extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_services);
 
-        /// do we wanna load shit from savedinstance
+        // load things from onSavedInstance on orientation change
+        loadSavedInstance(savedInstanceState);
 
+    }
 
+    private void loadSavedInstance(Bundle savedInstanceState) {
 
+        if (savedInstanceState != null && savedInstanceState.containsKey("RETRIEVE_LINKS")) {
+            if (listOfShows == null || listOfShows.size() == 0) {
+
+                int len = savedInstanceState.getInt("SIZE_OF_LINKS");
+
+                for (int i = 0; i < len; i++) {
+                    // what are we getting from the savedinstance????
+                    String title = savedInstanceState.getString("UNIQUE_ID" + i + "0");
+                    String img_url = savedInstanceState.getString("UNIQUE_ID" + i + "1");
+                    String description = savedInstanceState.getString("UNIQUE_ID" + i + "2");
+                    int year = savedInstanceState.getInt("UNIQUE_ID" + i + "3");
+                    double rating = savedInstanceState.getDouble("UNIQUE_ID" + i + "4");
+
+                    Show add_show = new Show(title, description, img_url, rating, year);
+
+                    listOfShows.add(add_show);
+                }
+            }
+        }
+        else {
+            //nothing
+            listOfShows = new ArrayList<>();
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle saveThis) {
+
+        int len = listOfShows == null? 0: listOfShows.size();
+        saveThis.putInt("NUM_LINKS_TO_RETRIEVE", len);
+        for(int i =0; i < len; i++) {
+            saveThis.putString("UNIQUE_ID"+ i + "0", listOfShows.get(i).getName());
+            saveThis.putString("UNIQUE_ID" + i + "1", listOfShows.get(i).getPicture());
+            saveThis.putString("UNIQUE_ID" + i + "2", listOfShows.get(i).getDescription());
+            saveThis.putInt("UNIQUE_ID" + i + "3", listOfShows.get(i).getYear());
+            saveThis.putDouble("UNIQUE_ID" + i + "4", listOfShows.get(i).getRating());
+
+        }
+        super.onSaveInstanceState(saveThis);
+    }
+
+    private void setUpRecycler(){
+        showRecycler = findViewById(R.id.recyclerView);
+        showRecycler.setHasFixedSize(true);
+
+        // set layout
+        showRecycler.setLayoutManager(new LinearLayoutManager(this));
+        //set adapter
+        adapter = new ShowAdapter(listOfShows, this);
+        showRecycler.setAdapter(adapter);
+
+        // set the listener for recycler view
+        RecycleViewClickListener listener = new RecycleViewClickListener() {
+            @Override
+            public void onLinkClick(int position) {
+                // add functionality for opening alert dialog box to display information
+            }
+        };
+        adapter.setListenerLink(listener);
+
+        // add divided b/w links
+        DividerItemDecoration decor = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        showRecycler.addItemDecoration(decor);
+
+        // do we have swipe functionality for our recycler???????????????
     }
 }
