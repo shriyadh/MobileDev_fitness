630298fc2aa4d2aac87d2022e3fb8f961210f0b4
Mariah Maynard <98000652+mariah-may23@users.noreply.github.com>
2023-02-15 13:24:26 -0500

    able to connect with api, changes made to manifest to access internet

--- app/src/main/java/edu/northeastern/team1/WebServices.java
+++ app/src/main/java/edu/northeastern/team1/WebServices.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -20,52 +21,72 @@ import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class WebServices extends AppCompatActivity {
-    // store shows from query
+    private Handler textHandler = new Handler();
+
+
+    private static final String TAG = "WebServiceActivity";
+
+
     ArrayList<String> shows = new ArrayList<>();
-
-    //textedit holds user's show
-    private EditText mURLEditText;
-
-    //api link
-    private String API = "https://api.tvmaze.com/search/shows?q=%s";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_services);
 
-        // find id of the show
-       // mURLEditText = (EditText)findViewById(R.id.URL_et);
     }
 
     public void callApi(View v){
         // store users show
        // String str = mURLEditText.getText().toString();
-        String search = "https://api.tvmaze.com/search/shows?q=sunny%20in";
-
+        //String str = mURLEditText.getText().toString();
+        String str = "https://api.tvmaze.com/search/shows?q=sunny%20in";
         runnableThread runnableThread = new runnableThread();
-        runnableThread.setData(search);
+        runnableThread.setURL(str);
         new Thread(runnableThread).start();
     }
 
+
     class runnableThread implements Runnable {
         // name, picture, premiere date year, description, rating
-        private String data;
-        String[] results = new String[2];
+        private String search;
 
-        public void setData(String _data) {
-            this.data = _data;
+        public void setURL(String data) {
+            this.search = data;
         }
 
         @Override
         public void run() {
 
+            try {
+                URL url = new URL(search);
+                System.out.println(url);
+
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                System.out.println("first");
+                conn.setRequestMethod("GET");
+                conn.setDoInput(true);
+
+                conn.connect();
+                System.out.println("Connected");
+
+            } catch (MalformedURLException e) {
+                Log.e(TAG, "MalformedURLException");
+                e.printStackTrace();
+            } catch (ProtocolException e) {
+                Log.e(TAG, "ProtocolException");
+                e.printStackTrace();
+            } catch (IOException e) {
+                Log.e(TAG, "IOException");
+                e.printStackTrace();
+            }
         }
+
+
     }
-
-
 
 
 
