e587446d9a074721ef7dfce82e83428c99ae191b
shriyadh03 <shriyadh03.gmail.com>
2023-02-15 13:19:35 -0500

    added recycler view id

--- app/src/main/java/edu/northeastern/team1/ShowRecycler.java
+++ app/src/main/java/edu/northeastern/team1/ShowRecycler.java
@@ -1,4 +1,30 @@
 package edu.northeastern.team1;
 
-public class ShowRecycler {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShowRecycler extends AppCompatActivity {
+
+    private RecyclerView showRecycler;
+    private List<Show> listOfShows = new ArrayList<>();
+
+    private RecycleViewClickListener listener;
+    private ShowAdapter adapter;
+    private RecyclerView.LayoutManager layout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_services);
+
+        /// do we wanna load shit from savedinstance
+
+
+
+    }
 }
