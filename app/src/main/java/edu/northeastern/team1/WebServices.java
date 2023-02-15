b9dfab72683ef0a26d97035b12b8a6bf59197670
mitchneides <mitch.neides@gmail.com>
2023-02-15 18:56:17 -0500

    Confirms user wants to leave on back press

--- app/src/main/java/edu/northeastern/team1/WebServices.java
+++ app/src/main/java/edu/northeastern/team1/WebServices.java
@@ -8,6 +8,8 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.text.Layout;
@@ -263,6 +265,27 @@ public class WebServices extends AppCompatActivity {
         showRecycler.addItemDecoration(decor);
 
         // do we have swipe functionality for our recycler???????????????
+    }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(false);
+        builder.setMessage("Are you sure you want to leave?\nResults will not be saved");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 }
 
