90e0be9dadf7aa3898436e7efb5aa386237afd71
mitchneides <mitch.neides@gmail.com>
2023-02-17 15:06:05 -0500

    Code cleanup

--- app/src/main/java/edu/northeastern/team1/WebServices.java
+++ app/src/main/java/edu/northeastern/team1/WebServices.java
@@ -34,11 +34,8 @@ import java.util.Scanner;
 public class WebServices extends AppCompatActivity {
     private RecyclerView showRecycler;
     private List<Show> listOfShows = new ArrayList<>();
-    private RecycleViewClickListener listener;
     private ShowAdapter adapter;
-    private RecyclerView.LayoutManager layout;
     private EditText searchBar;
-    private ImageButton magnifyingGlass;
     private String url;
     private final Handler handler = new Handler();
 
@@ -53,7 +50,6 @@ public class WebServices extends AppCompatActivity {
         findViewById(R.id.loadingPanel).setVisibility(View.GONE);
 
         this.searchBar = findViewById(R.id.EditText_TV_searchbar);
-        this.magnifyingGlass = findViewById(R.id.imageButton_magnifying_glass);
 
         SwitchCompat filterSwitch = findViewById(R.id.year_new_to_old_switch);
         filterSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
@@ -81,12 +77,11 @@ public class WebServices extends AppCompatActivity {
         callApi(v);
     }
 
-    public void callApi(View v){
+    public void callApi(View v) {
         runnableThread runnableThread = new runnableThread();
         runnableThread.setURL(url);
         new Thread(runnableThread).start();
     }
-
 
 
     class runnableThread implements Runnable {
@@ -112,17 +107,15 @@ public class WebServices extends AppCompatActivity {
 
                 conn.connect();
 
-
                 // get input from api and convert to string
                 InputStream inputStream = conn.getInputStream();
                 final String resp = StreamToString(inputStream);
-
 
                 // Store array of objects that api returns
                 JSONArray jArray = new JSONArray(resp);
 
                 // loop through the array
-                for (int i = 0; i < jArray.length(); i++ ) {
+                for (int i = 0; i < jArray.length(); i++) {
                     // store the first object in the array
                     JSONObject obj = jArray.getJSONObject(i);
 
@@ -137,15 +130,18 @@ public class WebServices extends AppCompatActivity {
                     String name = show.getString("name");
 
                     // get show description
-                    String description = show.getString("summary").replace("null", "N/A");
+                    String description = show.getString("summary")
+                                             .replace("null", "N/A");
 
                     // get average rating
                     JSONObject rating = show.getJSONObject("rating");
-                    String avg_rating = rating.getString("average").replace("null", "N/A");
+                    String avg_rating = rating.getString("average")
+                                              .replace("null", "N/A");
 
 
                     // get image link
-                    String img_link = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/2048px-No_image_available.svg.png";
+                    String img_link = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/" +
+                                      "No_image_available.svg/2048px-No_image_available.svg.png";
                     try {
                         JSONObject image = show.getJSONObject("image");
                         img_link = image.getString("medium");
@@ -155,7 +151,8 @@ public class WebServices extends AppCompatActivity {
 
                     //get premiere date
 
-                    String date = show.getString("premiered").replace("null", "N/A");
+                    String date = show.getString("premiered").replace("null",
+                                        "N/A");
                     String year = date.equals("N/A") ?
                             "N/A" : date.split("-")[0];
 
@@ -197,8 +194,7 @@ public class WebServices extends AppCompatActivity {
 
                 listOfShows.add(add_show);
             }
-        }
-        else {
+        } else {
             //nothing
             listOfShows = new ArrayList<>();
         }
@@ -206,10 +202,10 @@ public class WebServices extends AppCompatActivity {
 
     @Override
     protected void onSaveInstanceState(@NonNull Bundle saveThis) {
-        int len = listOfShows == null? 0: listOfShows.size();
+        int len = listOfShows == null ? 0 : listOfShows.size();
 
         saveThis.putInt("SIZE_OF_LINKS", len);
-        for(int i =0; i < len; i++) {
+        for (int i = 0; i < len; i++) {
             saveThis.putDouble("UNIQUE_ID" + i + "0", listOfShows.get(i).getScore());
             saveThis.putString("UNIQUE_ID" + i + "1", listOfShows.get(i).getName());
             saveThis.putString("UNIQUE_ID" + i + "2", listOfShows.get(i).getPicture());
@@ -221,7 +217,7 @@ public class WebServices extends AppCompatActivity {
         super.onSaveInstanceState(saveThis);
     }
 
-    private void setUpRecycler(){
+    private void setUpRecycler() {
         showRecycler = findViewById(R.id.recyclerView);
         showRecycler.setHasFixedSize(true);
 
@@ -244,7 +240,8 @@ public class WebServices extends AppCompatActivity {
         adapter.setListenerLink(listener);
 
         // add divided b/w links
-        DividerItemDecoration decor = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        DividerItemDecoration decor = new DividerItemDecoration(this,
+                                                                DividerItemDecoration.VERTICAL);
         showRecycler.addItemDecoration(decor);
 
     }
