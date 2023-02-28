336f06102a2a7ff5024446980a909c2b6c07b8a2
Gino <romangino@gmail.com>
2023-02-28 14:19:42 -0500

    Firebase Progress.

--- app/src/main/java/edu/northeastern/team1/FirebaseActivity.java
+++ app/src/main/java/edu/northeastern/team1/FirebaseActivity.java
@@ -13,15 +13,9 @@ public class FirebaseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_firebase);
+
         // Obtain the FirebaseAnalytics instance.
         FirebaseAnalytics mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
-
-        Bundle bundle = new Bundle();
-        String id = "1";
-        bundle.putString(FirebaseAnalytics.Param.ITEM_ID, id);
-        String name = "Log Event";
-        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, name);
-        bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "image");
-        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
+        mFirebaseAnalytics.logEvent("AHHHHHHHH", null);
     }
 }
