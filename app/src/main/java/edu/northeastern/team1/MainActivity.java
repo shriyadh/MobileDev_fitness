f2d4db9ff591c7bbd37e837527ae0cfbdc859b80
shriyadh03 <shriyadh03.gmail.com>
2023-02-28 12:35:10 -0500

    added about btn + activity for firebase

--- app/src/main/java/edu/northeastern/team1/MainActivity.java
+++ app/src/main/java/edu/northeastern/team1/MainActivity.java
@@ -18,4 +18,9 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, WebServices.class);
         startActivity(intent);
     }
+
+    public void firebaseActivity(View view) {
+        Intent intent = new Intent(this, FirebaseActivity.class);
+        startActivity(intent);
+    }
 }
