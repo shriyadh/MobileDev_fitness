34ec6998905a446359bf9ba9a57003d214777ca2
Mariah Maynard <98000652+mariah-may23@users.noreply.github.com>
2023-02-28 16:57:37 -0500

    checking for users works

--- app/src/main/java/edu/northeastern/team1/FirebaseActivity.java
+++ app/src/main/java/edu/northeastern/team1/FirebaseActivity.java
@@ -10,9 +10,13 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Logger;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class FirebaseActivity extends AppCompatActivity {
@@ -27,19 +31,37 @@ public class FirebaseActivity extends AppCompatActivity {
 
         // Write a message to the database
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("users");
+        //DatabaseReference myRef = database.getReference("users");
         curUser = findViewById(R.id.username);
 
 
-        myRef.setValue("Mariah!");
+       // myRef.setValue("Mariah!");
     }
 
     public void logIn(View view) {
         String user = String.valueOf(curUser.getText()); //current username
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference getUsers = database.getReference("users"); // ref to all the users
-        DatabaseReference current = getUsers.child(user);
-        Log.v("CUR", String.valueOf(current));
+
+        DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference userNameRef = rootRef.child("testlogin").child(user);
+        ValueEventListener eventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(!dataSnapshot.exists()) {
+                    //create new user
+                    Log.v("CUR", String.valueOf(" notHERE"));
+                }
+                else{
+                    Log.v("CUR", String.valueOf("HERE"));
+
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.d("tag", databaseError.getMessage()); //Don't ignore errors!
+            }
+        };
+        userNameRef.addListenerForSingleValueEvent(eventListener);
 
 
     }
