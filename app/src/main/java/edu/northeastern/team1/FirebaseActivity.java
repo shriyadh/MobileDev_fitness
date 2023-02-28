181931f934a0a38d68bfd5e626aeb615a89403c6
Gino <romangino@gmail.com>
2023-02-28 13:25:15 -0500

    Progress in adding Firebase

--- app/src/main/java/edu/northeastern/team1/FirebaseActivity.java
+++ app/src/main/java/edu/northeastern/team1/FirebaseActivity.java
@@ -1,8 +1,11 @@
 package edu.northeastern.team1;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.firebase.analytics.FirebaseAnalytics;
+
 
 public class FirebaseActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class FirebaseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_firebase);
+        // Obtain the FirebaseAnalytics instance.
+        FirebaseAnalytics mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
+        Bundle bundle = new Bundle();
+        String id = "1";
+        bundle.putString(FirebaseAnalytics.Param.ITEM_ID, id);
+        String name = "Log Event";
+        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, name);
+        bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "image");
+        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
     }
 }
--- app/src/main/java/edu/northeastern/team1/MainActivity.java
+++ app/src/main/java/edu/northeastern/team1/MainActivity.java
@@ -1,10 +1,10 @@
 package edu.northeastern.team1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
