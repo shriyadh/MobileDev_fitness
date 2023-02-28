b28fe1d6fff251a8a797891d8e0a90f519012ea9
shriyadh03 <shriyadh03.gmail.com>
2023-02-28 12:48:15 -0500

    Added about activity

---
+++ app/src/main/java/edu/northeastern/team1/AboutTeam.java
@@ -0,0 +1,14 @@
+package edu.northeastern.team1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AboutTeam extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_team);
+    }
+}
