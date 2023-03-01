b714f94f794ca45d9472a1f04b5664608d227c45
Mariah Maynard <98000652+mariah-may23@users.noreply.github.com>
2023-03-01 16:43:11 -0500

    added criteria to for new users

--- app/src/main/java/edu/northeastern/team1/FirebaseActivity.java
+++ app/src/main/java/edu/northeastern/team1/FirebaseActivity.java
@@ -19,6 +19,9 @@ import com.google.firebase.database.Logger;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 public class FirebaseActivity extends AppCompatActivity {
     private EditText curUser;
@@ -31,8 +34,39 @@ public class FirebaseActivity extends AppCompatActivity {
         curUser = findViewById(R.id.username);
     }
 
+    public String createUser(String user){
+        // checking for special characters
+        // https://stackoverflow.com/questions/1795402/check-if-a-string-contains-a-special-character
+        Pattern special = Pattern.compile ("[!,@#$%&*()+=|<>?{}\\[\\]~-]");
+        Matcher m = special.matcher(user);
+
+        if(m.find()) { // if any special characters found
+            Toast.makeText(getApplicationContext() , "Username cannot contain any special characters!", Toast.LENGTH_SHORT).show();
+            return null;
+        }
+        //check if username is all numbers
+        boolean digits = allNumbers(user);
+        if(digits){ // username was all numbers
+            Toast.makeText(getApplicationContext() , "Username must have at least one letter!", Toast.LENGTH_SHORT).show();
+            return null;
+        }
+        //check for username with spaces
+        if(user.contains(" ")) {
+            this.user = this.user.replaceAll("\\s+", "_"); // add hyphen if there are spaces
+            Toast.makeText(getApplicationContext() , "Your username has been changed to: " + this.user, Toast.LENGTH_SHORT).show();
+            return this.user;
+        }
+        return user;
+
+    }
+
     public void logIn(View view) {
         this.user = String.valueOf(curUser.getText()); //current username
+        String verifiedName = createUser(user);
+        if (verifiedName==null){
+            return;
+        }
+
         DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
         DatabaseReference userName = rootRef.child("testlogin").child(user);
 
@@ -43,6 +77,7 @@ public class FirebaseActivity extends AppCompatActivity {
                 if(!dataSnapshot.exists()) {
                     //create new user
                     rootRef.child("testlogin").child(user).setValue("");
+
                 }
             }
 
@@ -65,5 +100,19 @@ public class FirebaseActivity extends AppCompatActivity {
 
 
     }
+
+    // geeksforgeeks
+    public boolean allNumbers(String name){
+
+        for (int i = 0; i < name.length(); i++) {
+
+            if (!Character.isDigit(name.charAt(i))) {
+                return false;
+            }
+        }
+        // all characters were numbers
+        return true;
+    }
+
 
 }
